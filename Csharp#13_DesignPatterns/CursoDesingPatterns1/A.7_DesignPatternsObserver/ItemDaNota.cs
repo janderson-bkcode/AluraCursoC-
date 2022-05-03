21528b7eea3c4e4fffc87f28fc6535797b259921
janderson-bkbankcode <janderson.goncalves@bkbank.com.br>
2022-05-03 15:04:16 -0300

    Criação Aula07

---
+++ Csharp#13_DesignPatterns/CursoDesingPatterns1/A.7_DesignPatternsObserver/ItemDaNota.cs
@@ -0,0 +1,16 @@
+﻿namespace A._7_DesignPatternsObserver
+{
+    public class ItemDaNota
+    {
+        public string Descricao { get; set; }
+        public double Valor { get; set; }
+
+        public ItemDaNota(string descricao, double valor)
+        {
+            Descricao = descricao;
+            Valor = valor;
+        }
+
+
+    }
+}
