5cc5881b6dd2f463d569aa1146d86f39a7271b27
janderson-bkbankcode <janderson.goncalves@bkbank.com.br>
2022-05-03 16:19:28 -0300

    create Multiplicador.cs
    Exercicio aula 07

---
+++ Csharp#13_DesignPatterns/CursoDesingPatterns1/A.7_DesignPatternsObserver/Multiplicador.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A._7_DesignPatternsObserver
+{
+    public class Multiplicador : AcaoAposGerarNotaObserver
+    {
+        public double Fator { get; private set; }
+
+        public Multiplicador(double fator) { this.Fator = fator; }
+
+        public void Executa(NotaFiscal notaFiscal)
+        {
+            Console.WriteLine(notaFiscal.ValorBruto * this.Fator);
+        }
+    }
+
+    class Teste
+    {
+        static void Main(String[] args)
+        {
+            NotaFiscalBuilder builder = new NotaFiscalBuilder();
+            builder.AdicionaAcao(new Multiplicador(2));
+            builder.AdicionaAcao(new Multiplicador(3));
+            builder.AdicionaAcao(new Multiplicador(5.5));
+
+            NotaFiscal notaFiscal = builder.ParaEmpresa("Caelum")
+                .ComCnpj("123.456.789/0001-10")
+                .Com(new ItemDaNota("item 1", 100.0))
+                .Com(new ItemDaNota("item 2", 200.0))
+                .Com(new ItemDaNota("item 3", 300.0))
+                .ComObservacoes("entregar notaFiscal pessoalmente")
+                .NaDataAtual()
+                .Constroi();
+        }
+    }
+}
