fa699d86cb22b5b1a3d88bf66718e48b986aabad
janderson-bkbankcode <janderson.goncalves@bkbank.com.br>
2022-05-04 16:12:05 -0300

    update 04/05/22 aula 03
    update

--- Csharp#13_DesignPatterns/CursoDesingPatterns1/A.3_DesignPatternsTemplateMethod/TesteDataMiner/CSVDataMiner.cs
+++ Csharp#13_DesignPatterns/CursoDesingPatterns1/A.3_DesignPatternsTemplateMethod/TesteDataMiner/CSVDataMiner.cs
@@ -6,21 +6,13 @@ using System.Threading.Tasks;
 
 namespace A._3_DesignPatternsTemplateMethod.TesteDataMiner
 {
-    public class PDFDataMiner : DataMiner
-
+    public class CSVDataMiner : DataMiner
     {
-        public String Extensao { get; set; }
-
-        public String Nome { get; set; }
-
-        public Path Caminho { get; set; }
-
         protected override void closeFile(Path path)
         {
             throw new NotImplementedException();
         }
 
-   
         protected override void extractData(String file)
         {
             throw new NotImplementedException();
@@ -28,13 +20,12 @@ namespace A._3_DesignPatternsTemplateMethod.TesteDataMiner
 
         protected override void openFile(Path path)
         {
-            Caminho = path;
+            throw new NotImplementedException();
         }
 
         protected override void parseData()
         {
             throw new NotImplementedException();
         }
-
     }
 }
