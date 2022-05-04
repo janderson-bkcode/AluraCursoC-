71f2494e3faaabb96ff7984e4c40756e232b832a
janderson-bkbankcode <janderson.goncalves@bkbank.com.br>
2022-05-04 13:42:45 -0300

    update 04/05
    Adição de classe que reprensenta o imposto IOF como teste da aula 01

---
+++ Csharp#13_DesignPatterns/CursoDesingPatterns1/CursoDesingPatterns1/StrategyImposto/IOF.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoDesingPatterns1.StrategyImposto
+{
+    public class IOF : ImpostoStrategyInterface
+    {
+        public double Calcula(OrcamentoStrategyClient orcamento)
+        {
+            return orcamento.Valor * 0.0082;
+        }
+    }
+}
