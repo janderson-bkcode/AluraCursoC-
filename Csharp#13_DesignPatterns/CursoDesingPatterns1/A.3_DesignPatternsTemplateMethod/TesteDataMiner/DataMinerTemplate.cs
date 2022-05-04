1705e2917c815dc8a1b3eb1f9ccce94051b40588
janderson-bkbankcode <janderson.goncalves@bkbank.com.br>
2022-05-04 16:07:59 -0300

    update
    Criação da pasta TesteDataMiner
    Teste aula 03

---
+++ Csharp#13_DesignPatterns/CursoDesingPatterns1/A.3_DesignPatternsTemplateMethod/TesteDataMiner/DataMinerTemplate.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A._3_DesignPatternsTemplateMethod.TesteDataMiner
+{
+    public abstract class DataMiner
+    {
+
+        public String File { get; set; }
+        public void mine(Path path)
+        {
+            openFile(path);
+            extractData(File);
+            parseData();
+            analyze();
+            sendReport();
+            closeFile(path);
+        }
+        protected abstract void openFile(Path path);
+        protected abstract void extractData(String file);
+        protected abstract void parseData();
+
+        protected void analyze()
+        {
+            Console.WriteLine("Análise");
+        }
+        protected void sendReport()
+        {
+            Console.WriteLine("Reportando Analise");
+        }
+        protected abstract void closeFile(Path path);
+
+    }
+}
---
+++ Csharp#13_DesignPatterns/CursoDesingPatterns1/A.3_DesignPatternsTemplateMethod/TesteDataMiner/Path.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace A._3_DesignPatternsTemplateMethod.TesteDataMiner
+{
+    public class Path
+    {
+
+        public String caminhoUrl { get; set; }
+    }
+}
