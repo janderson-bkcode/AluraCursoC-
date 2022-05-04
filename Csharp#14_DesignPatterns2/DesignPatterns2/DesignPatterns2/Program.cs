e97bb48b9af4930e4032ee23d6e86cdc344022a5
janderson-bkbankcode <janderson.goncalves@bkbank.com.br>
2022-05-04 16:59:27 -0300

    Criação aula 01
    Criação da aula 01 Factory Design pattern2

--- Csharp#14_DesignPatterns2/DesignPatterns2/DesignPatterns2/Program.cs
+++ Csharp#14_DesignPatterns2/DesignPatterns2/DesignPatterns2/Program.cs
@@ -12,14 +12,15 @@ namespace DesignPatterns2.Aula01
     {
         static void Main(string[] args)
         {
-            //Abrindo conexão
-            IDbConnection conexao = new SqlConnection();
-            conexao.ConnectionString = "User Id =root;Password=;Server=localhost;Database=meuBanco";
-            conexao.Open();
+            //Abrindo conexão usando a classe ConnectionFactory
+            IDbConnection conexao = new ConnectionFactory().GetConnection();
 
             //Variavel que representa o comando a ser executado
             IDbCommand comando =  conexao.CreateCommand();
             comando.CommandText = "select * from tabela;";
+
+
+
 
         }
     }
