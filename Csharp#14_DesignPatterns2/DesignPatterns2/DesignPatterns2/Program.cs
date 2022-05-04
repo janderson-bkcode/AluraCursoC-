e4660ed38e8c61d629555b852ff6dcf01dfcace3
janderson-bkbankcode <janderson.goncalves@bkbank.com.br>
2022-05-04 16:49:35 -0300

    update

---
+++ Csharp#14_DesignPatterns2/DesignPatterns2/DesignPatterns2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns2.Aula01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Abrindo conexão
+            IDbConnection conexao = new SqlConnection();
+            conexao.ConnectionString = "User Id =root;Password=;Server=localhost;Database=meuBanco";
+            conexao.Open();
+
+            //Variavel que representa o comando a ser executado
+            IDbCommand comando =  conexao.CreateCommand();
+            comando.CommandText = "select * from tabela;";
+
+        }
+    }
+}
