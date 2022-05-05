bf1cdeb1c2e5120bff8baefd4bb203045db63c02
janderson-bkbankcode <janderson.goncalves@bkbank.com.br>
2022-05-05 14:24:35 -0300

    update 05/05/2022
    Aula 04 finalizada de fato

---
+++ Csharp#14_DesignPatterns2/DesignPatterns2/A.4_DesignPatternDSL_Interpreter/RaizQuadrada.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ Implemente uma expressão que calcula a raíz quadrada do número. 
+ Para calcular, basta usar o método Math.Sqrt().
+ Uma raiz quadrada calcula o valor em cima de uma expressão.
+ Cole seu código aqui.
+ */
+namespace A._4_DesignPatternDSL_Interpreter
+{
+    public class RaizQuadrada : IExpressao
+    {
+        private IExpressao expressao;
+
+        public RaizQuadrada(IExpressao e)
+        {
+            this.expressao = e;
+        }
+
+        public int Avalia()
+        {
+            return (int)Math.Sqrt(expressao.Avalia());
+        }
+    }
+}
