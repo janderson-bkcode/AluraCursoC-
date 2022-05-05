7e420f269ec75b374f53d1b36634d16b1295ced3
janderson-bkbankcode <janderson.goncalves@bkbank.com.br>
2022-05-05 17:18:16 -0300

    Aula 08 finalizada

---
+++ Csharp#14_DesignPatterns2/DesignPatterns2/A.8_DesignPatternsAdapter/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace A._8_DesignPatternsAdapter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Cliente cliente = new Cliente();
+            cliente.Nome = "victor";
+            cliente.Endereco = "Rua Vergueiro";
+            cliente.DataDeNascimento = DateTime.Now;
+
+            XmlSerializer serializer = new XmlSerializer(cliente.GetType());
+            StringWriter writer = new StringWriter();
+            serializer.Serialize(writer, cliente);
+            String xml = writer.ToString();
+
+
+            Cliente cliente2 = new Cliente();
+            cliente.Nome = "victor";
+            cliente.Endereco = "Rua Vergueiro";
+            cliente.DataDeNascimento = DateTime.Now;
+
+            GeradorDeXml gerador = new GeradorDeXml();
+            String xml2 = gerador.GeraXml(cliente);
+        }
+    }
+}
