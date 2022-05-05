3350ccd1497624eb5bc4dfdd4f197ed25b748597
janderson-bkbankcode <janderson.goncalves@bkbank.com.br>
2022-05-05 14:30:40 -0300

    update Aula 05
    Copiando e colando classes da aula 04 para aula 05

---
+++ Csharp#14_DesignPatterns2/DesignPatterns2/A.5_DesignPatternVisitor/Divisao.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A._5_DesignPatternVisitor
+{
+    public class Divisao : IExpressao
+    {
+        private IExpressao esquerda;
+        private IExpressao direita;
+
+        public Divisao(IExpressao esquerda, IExpressao direita)
+        {
+            this.esquerda = esquerda;
+            this.direita = direita;
+        }
+
+        public int Avalia()
+        {
+            int resultadoDaEsquerda = esquerda.Avalia();
+            int resultadoDaDireita = direita.Avalia();
+            return resultadoDaEsquerda / resultadoDaDireita;
+        }
+    }
+
+}
---
+++ Csharp#14_DesignPatterns2/DesignPatterns2/A.5_DesignPatternVisitor/Numero.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A._5_DesignPatternVisitor
+{
+    internal class Numero : IExpressao
+    {
+        private int numero;
+
+        public Numero(int numero)
+        {
+            this.numero = numero;
+        }
+
+        public int Avalia()
+        {
+            return this.numero;
+        }
+    }
+}
