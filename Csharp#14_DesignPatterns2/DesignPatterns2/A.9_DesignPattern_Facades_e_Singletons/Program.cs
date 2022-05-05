f6a9d57135b70668c10f8a4e02d5c8c867a8f823
janderson-bkbankcode <janderson.goncalves@bkbank.com.br>
2022-05-05 17:19:58 -0300

    Create Aula09
    Criação da Aula 09

---
+++ Csharp#14_DesignPatterns2/DesignPatterns2/A.9_DesignPattern_Facades_e_Singletons/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A._9_DesignPattern_Facades_e_Singletons
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
